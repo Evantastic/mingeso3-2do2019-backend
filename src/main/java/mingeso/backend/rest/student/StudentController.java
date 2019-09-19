f1b9ce411a8734c503cdc1da8f74ad5903990243
Alan Alejandro Barahona Ruiz <alan.barahona@usach.cl>
2019-09-19 01:50:12 -0300

    Updated CORS Origin in Student Controller

--- src/main/java/mingeso/backend/rest/student/StudentController.java
+++ src/main/java/mingeso/backend/rest/student/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/rest/students")
+@CrossOrigin(origins = "*")
 public class StudentController{
 
     private final StudentService studentService;
