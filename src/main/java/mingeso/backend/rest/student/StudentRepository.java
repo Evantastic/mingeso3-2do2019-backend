5c6b434b521abc6d6913d8def57416c10c21e379
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2019-10-03 18:06:20 -0300

    NEW: Project restarted

--- src/main/java/mingeso/backend/rest/student/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package mingeso.backend.rest.student;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends PagingAndSortingRepository<Student, Integer> {
-    Student findByRut(String rut);
-}
