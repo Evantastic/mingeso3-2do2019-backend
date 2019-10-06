25fff292c07ceb389d3f7496ec20bf5440f424fb
gabo <gabogsco@gmail.com>
2019-10-06 20:26:43 -0300

    NEW: Classes and Swagger

---
+++ src/main/java/mingeso/backend/rest/mysql/reserve/Reserve.java
@@ -0,0 +1,42 @@
+package mingeso.backend.rest.mysql.reserve;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Reserve")
+public class Reserve {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id", unique = true, nullable = false)
+    private int id;
+
+    @Column(name = "clientid", unique = false, nullable = false)
+    private int clientId;
+
+    @Column(name = "roomid", unique = false, nullable = false)
+    private int roomid;
+
+    @Column(name = "starDate")
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate startDate;
+
+    @Column(name = "endDate")
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate endDate;
+
+    @Column(name="price")
+    private int price;
+
+}
