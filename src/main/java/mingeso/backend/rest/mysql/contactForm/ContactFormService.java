6f38991991ea7c72628e402759e016ba613a4ab9
gabo <gabogsco@gmail.com>
2019-10-07 18:21:01 -0300

    NEW: ContactForm Service

---
+++ src/main/java/mingeso/backend/rest/mysql/contactForm/ContactFormService.java
@@ -0,0 +1,46 @@
+package mingeso.backend.rest.mysql.contactForm;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ContactFormService {
+
+    private final ContactFormRepository contactFormRepository;
+
+
+    public Slice<ContactForm> getAll(int page, int quantity) {
+        return contactFormRepository.findAll(PageRequest.of(page, quantity));
+    }
+
+    public ContactForm getById(int id) {
+        return contactFormRepository.findById(id)
+                .orElse(null);
+    }
+
+    public ContactForm create(ContactForm contactForm) {
+        return contactFormRepository.save(contactForm);
+    }
+
+    public ContactForm update(int id, ContactForm newContactForm) {
+        return contactFormRepository.findById(id)
+                .map(contactForm -> {
+                    contactForm.setFromContactForm(newContactForm);
+                    return contactFormRepository.save(contactForm);
+                })
+                .orElse(null);
+    }
+
+    public ContactForm delete(int id) {
+        return contactFormRepository.findById(id)
+                .map(contactForm -> {
+                    contactFormRepository.delete(contactForm);
+                    return contactForm;
+                })
+                .orElse(null);
+    }
+}
---
+++ src/main/java/mingeso/backend/rest/mysql/reserve/ReserveRepository.java
@@ -0,0 +1,7 @@
+package mingeso.backend.rest.mysql.reserve;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReserveRepository {
+}
