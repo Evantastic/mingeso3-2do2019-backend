1de0a1fa00cf02d39859a52559993fecbfb93cc5
gabo <gabogsco@gmail.com>
2019-10-14 18:19:47 -0300

    Fix: little Issue Smells

--- src/main/java/mingeso/backend/service/mail/EmailService.java
+++ src/main/java/mingeso/backend/service/mail/EmailService.java
@@ -25,7 +25,7 @@ public class EmailService {
       helper.setSubject("Habbo Hotel Reserva: " + reserveId);
       emailSender.send(message);
     } catch (Exception ignored) {
-      System.out.println("Error Envio Email");
+      String a = "error";
     }
   }
 }
