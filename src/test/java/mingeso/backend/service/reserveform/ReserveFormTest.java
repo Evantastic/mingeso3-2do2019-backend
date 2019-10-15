89f2e986b5032aed46b1d0a0564a541a537a2384
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2019-10-15 00:36:33 -0300

    FIX: fixed naming conventions

--- src/test/java/mingeso/backend/service/reserveform/ReserveFormTest.java
+++ src/test/java/mingeso/backend/service/reserveform/ReserveFormTest.java
@@ -6,13 +6,13 @@ import java.time.LocalDate;
 
 public class ReserveFormTest {
 
-    private final LocalDate STARTDATE = LocalDate.now();
-    private final LocalDate ENDDATE = LocalDate.now();
-    private final String NAME = "Jose Perez";
-    private final LocalDate BIRTH = LocalDate.now();
-    private final String EMAIL = "joseperez@gmail.com";
-    private final String PHONE = "4567891212";
-    private final int ROOMNUMBER = 101;
+    private final static LocalDate STARTDATE = LocalDate.now();
+    private final static LocalDate ENDDATE = LocalDate.now();
+    private final static String NAME = "Jose Perez";
+    private final static LocalDate BIRTH = LocalDate.now();
+    private final static String EMAIL = "joseperez@gmail.com";
+    private final static String PHONE = "4567891212";
+    private final static int ROOMNUMBER = 101;
 
     @Test
     public void WhenInitializeConstructorWithAllArguments(){
